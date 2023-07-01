24f3d21d164af4ccf37e64e6864c1f190a19c4e1
Liubov <gareevaliubov@gmail.com>
2023-07-01 14:35:22 +0300

    [master] инициализация репозитория

---
+++ project-1/wwwroot/scripts/main.js
@@ -0,0 +1,24 @@
+
+// получаем html элемент img
+const img = document.getElementById("img");
+
+// получаем html элемент button
+const button = document.getElementById("btn-img");
+
+// объект с текстом для button
+const text = {
+  show: "Показать картинку",
+  hide: "Скрыть картинку"
+};
+
+// функция которая будет вызвана при нажатии на кнопку
+button.onclick = function (ev) {
+    img.hidden = !img.hidden;
+
+    if (img.hidden) {
+        button.innerText = text.show;
+    }
+    else {
+        button.innerText = text.hide;
+    }
+};
