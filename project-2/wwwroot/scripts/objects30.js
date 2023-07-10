e2cc7208d3ab5a1d68b7deab168fd0e7ad2be821
Liubov <gareevaliubov@gmail.com>
2023-07-10 11:23:22 +0300

    [project-2] Решила задачу 30

--- project-2/wwwroot/scripts/objects30.js
+++ project-2/wwwroot/scripts/objects30.js
@@ -1,12 +1,37 @@
+// Проверка на спам
+
+// Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+//
+//     Функция должна быть нечувствительна к регистру:
+//
+//     checkSpam('buy ViAgRA now') == true
+// checkSpam('free xxxxx') == true
+// checkSpam("innocent rabbit") == false
+
+
 const objects30 = function () {
 
+    function checkSpam(str) {
+        str = str.toLowerCase();
+        let pos = 0;
+        let a = 'viagra';
+        let b = 'XXX'.toLowerCase();
+
+        let c = str.indexOf(a, pos);
+        let d = str.indexOf(b, pos);
+
+        if ((c > -1) || (d > -1)) {
+            return true;
+        }
+        return false;
+    }
+
+    alert(checkSpam('XXX'));             // true
+    alert(checkSpam('viagra'));          // true
+    alert(checkSpam('buy ViAgRA now'));  // true
+    alert(checkSpam('free xxxxx'));      // true
+    alert(checkSpam("innocent rabbit")); // false
+    alert(checkSpam('XxXxXXxxxxxxxx'));  // true
 
 
-
-
-
-
-
-
-    alert("Задача 30 не решена");
 };
