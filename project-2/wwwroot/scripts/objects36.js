f6d2b3c05c85422af550cdc95df99c5e04a6c705
Liubov <gareevaliubov@gmail.com>
2023-07-11 19:18:30 +0300

    [project-2] задачи по массивам

---
+++ project-2/wwwroot/scripts/objects36.js
@@ -0,0 +1,20 @@
+
+
+
+
+// Вызов функции из массива
+
+const objects36 = function () {
+
+let arr = [1, 2, 3, 4, 5];
+    alert(arr);
+
+    arr.push (function () {alert('Привет!')});
+
+     arr[5]();
+
+     let f = arr[5];
+
+     f();
+
+};
