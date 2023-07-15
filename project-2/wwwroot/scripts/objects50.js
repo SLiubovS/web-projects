b1d9e44205e5090c59a135cee86afde2acb34efe
Liubov <gareevaliubov@gmail.com>
2023-07-15 12:47:30 +0300

    [project-2] 50 разбор стрелочной функции в find

--- project-2/wwwroot/scripts/objects50.js
+++ project-2/wwwroot/scripts/objects50.js
@@ -1,9 +1,16 @@
-
+// тестирование метода find у массива
 
 
 const objects50 = function () {
 
+    let triangles = [
+        {x: 5, y: 1, z: 2},
+        {x: 3, y: 4, z: 8},
+        {x: 6, y: 7, z: 15}
+    ];
+
+    let result = triangles.find(obj => obj.z === 8);
 
 
-
+    alert(`x = ${result.x}, y = ${result.y}, z = ${result.z}`); // возвращаем элемент z  у которого равен 8
 };
