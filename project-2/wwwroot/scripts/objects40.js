61386da1e0c036a55a5f421aa4401917b5f0b078
Liubov <gareevaliubov@gmail.com>
2023-07-18 14:10:48 +0300

    [project-2] сделала 40 задачу

--- project-2/wwwroot/scripts/objects40.js
+++ project-2/wwwroot/scripts/objects40.js
@@ -9,7 +9,14 @@
 
 const objects40 = function () {
 
+    let arr = [5, 2, 1, -10, 8];
 
+    function sortDescending(a, b) {
+        if (a > b) return -1;
+        if (a === b) return 0;
+        if (a < b) return 1;
+    }
 
-
+    arr.sort(sortDescending);
+    alert(arr);
 };
