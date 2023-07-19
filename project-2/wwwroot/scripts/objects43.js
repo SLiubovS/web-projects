28e4970ea879eab2a0918560e5fe7a5f1c73102b
Liubov <gareevaliubov@gmail.com>
2023-07-19 23:02:13 +0300

    [project-2] сделала 41 задачу

--- project-2/wwwroot/scripts/objects43.js
+++ project-2/wwwroot/scripts/objects43.js
@@ -18,7 +18,37 @@
 
 const objects43 = function () {
 
+    let vasya = {name: "Вася", age: 25};
+    let petya = {name: "Петя", age: 30};
+    let masha = {name: "Маша", age: 28};
 
+    let users = [vasya, petya, masha];
+    let result = [];
+
+
+    function sortDescending(a, b) {
+        if (a.age > b.age) return -1;
+        if (a.age === b.age) return 0;
+        if (a.age < b.age) return 1;
+    }
+
+
+
+//     let names = users.filter(obj => obj.age > 24);
+//     for (let obj of names) {
+//         result.push(`${obj.name}`);
+//
+//         // alert (result); // не совсем то, выдает вася, потом вася, петя и на третьем разе вася, петя, маша
+//     }
+//
+//     for (obj of names) {
+// // let results = `${vasya.name}, ${petya.name}, ${masha.name}`;
+//         // alert (results); // выдает 3 раза массив вася, петя, маша....
+//     alert (`${obj.name}`);
+//     }
+
+
+// alert (`${names}`);
 
 
 };
