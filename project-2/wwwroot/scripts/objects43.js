6f6dfe38e89bf90cb8c3c3f605e246404aa9356a
Liubov <gareevaliubov@gmail.com>
2023-07-20 18:21:54 +0300

    [project-2] сделала 43

--- project-2/wwwroot/scripts/objects43.js
+++ project-2/wwwroot/scripts/objects43.js
@@ -23,21 +23,16 @@ const objects43 = function () {
     let masha = {name: "Маша", age: 28};
 
     let users = [vasya, petya, masha];
-    let names = [];
-    let fil = users.filter(item => item.age > 24);
-    for (item of fil) {
+    // мое решение
+    // let names = [];
+    // let fil = users.filter(item => item.age > 24);
+    // for (item of fil) {
+    //
+    //     names.push(`${item.name}`);
 
-        names.push(`${item.name}`);
-    }
-    alert (names.sort());
-    alert (names);
+    // }
 
-  // function sortDescending(a, b) {
-  //       if (a.age > b.age) return 1;
-  //       if (a.age === b.age) return 0;
-  //       if (a.age < b.age) return -1;
-  //   }
-  //
-  // alert (names.sort(sortDescending));
-  // alert (names);
+    // решение Димы
+    let names = users.map(item => item.name);
+    alert(names);
 };
