99bd0273ce109c803541ff4a6f16ca51c38719be
Liubov <gareevaliubov@gmail.com>
2023-07-21 13:04:26 +0300

    [project-2] сделала 47 задачу

--- project-2/wwwroot/scripts/objects44.js
+++ project-2/wwwroot/scripts/objects44.js
@@ -31,7 +31,22 @@
 
 const objects44 = function () {
 
+    let vasya = {name: "Вася", surname: "Пупкин", id: 1};
+    let petya = {name: "Петя", surname: "Иванов", id: 2};
+    let masha = {name: "Маша", surname: "Петрова", id: 3};
+
+    let users = [vasya, petya, masha];
+
+// через функцию массива map из item сконструировать целевой (новый) объект
+
+    let usersMapped = users.map(item => {
+        return {fullName: item.name + " " + item.surname, id: item.id};
+    })
 
 
-
+    for (let i of usersMapped) {
+        alert(`${i.fullName}, ${i.id}`);
+    }
+    alert(usersMapped[0].id)
+    alert(usersMapped[0].fullName)
 };
--- project-2/wwwroot/scripts/objects47.js
+++ project-2/wwwroot/scripts/objects47.js
@@ -18,25 +18,29 @@
 
 const objects47 = function () {
 
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 29 };
+    let vasya = {name: "Вася", age: 25};
+    let petya = {name: "Петя", age: 30};
+    let masha = {name: "Маша", age: 29};
 
-let arr = [ vasya, petya, masha ];
+    let arr = [vasya, petya, masha];
 
-function getAverageAge(arr) {
+    function getAverageAge(arr) {
 
+        let array = [];
+        let result = +'';
 
+        for (let item of arr) {
+            if (item.age) {
+                array.push(item.age);
+            }
 
+        }
+        for (let items of array) {
+            result += items;
+        }
+        let results = result / array.length;
 
-
-
-
-
-}
-    getAverageAge(arr);
-
-
-
-
+        return results;
+    }
+    alert( getAverageAge(arr) );
 };
