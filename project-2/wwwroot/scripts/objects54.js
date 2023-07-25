c40398319451618652a460855f81e62c345435ba
Liubov <gareevaliubov@gmail.com>
2023-07-25 12:25:27 +0300

    [project-2] сделала 54

--- project-2/wwwroot/scripts/objects54.js
+++ project-2/wwwroot/scripts/objects54.js
@@ -1,12 +1,38 @@
-
+// Сумма свойств объекта
+// важность: 5
+// Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
+//
+//     Напишите функцию sumSalaries(salaries), которая возвращает сумму всех зарплат с помощью метода Object.values и цикла for..of.
+//
+//     Если объект salaries пуст, то результат должен быть 0.
+//
+// Например:
+//
+//     let salaries = {
+//         "John": 100,
+//         "Pete": 300,
+//         "Mary": 250
+//     };
+//
+// alert( sumSalaries(salaries) ); // 650
 
 
 const objects54 = function () {
 
+    let salaries = {
+        "John": 100,
+        "Pete": 300,
+        "Mary": 250
+    };
 
+    function sumSalaries(arr) {
+        let sum = 0;
+        let array = Object.values(salaries);
+        for (let num of array) {
+            sum += num;
+        }
+        return sum;
+    }
 
-
-
-
-
+    alert(sumSalaries(salaries));
 };
