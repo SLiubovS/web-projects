9579ecaed8c9d6fe75a908996e204d5c5970061a
Liubov <gareevaliubov@gmail.com>
2023-08-14 19:06:39 +0300

    [project-3] создали 3 проект

---
+++ project-3/wwwroot/scripts/main.js
@@ -0,0 +1,15 @@
+
+
+// тестовый код в качестве примера
+
+// function pendingAction() {
+//     let p = document.getElementById("hello-world-text");
+//
+//     p.innerHTML = "<i>Привет, Дима!</i>";
+// }
+//
+// setTimeout(pendingAction, 5000);
+
+
+
+
