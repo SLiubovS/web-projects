6d3a5c0e569bb5479cc7ad4281ac22aaeda895a6
Liubov <gareevaliubov@gmail.com>
2023-08-15 13:21:00 +0300

    [project-3] сделала 3 проект

--- project-3/wwwroot/scripts/main.js
+++ project-3/wwwroot/scripts/main.js
@@ -12,4 +12,26 @@
 
 
 
+// 1. Таблицу с id="age-table". (вводим в консоль следующий код и находит)
 
+document.getElementById('age-table');
+
+// 2. Все элементы label внутри этой таблицы (их три).
+
+document.querySelectorAll("td > label");
+
+// 3. Первый td в этой таблице (со словом «Age»).
+
+document.querySelector("td:first-child");
+
+// 4. Форму form с именем name="search".
+
+document.querySelector("form"); // не понимаю, я правильно нашла или нет=)
+
+// 5. Первый input в этой форме.
+
+document.querySelector("input:first-child");
+
+// 6. Последний input в этой форме.
+
+document.querySelector("form > input:last-child");
