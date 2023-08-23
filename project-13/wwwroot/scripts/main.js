0b0a0f1d248a0dd60c951dd4b0ea398fd3052847
Liubov <gareevaliubov@gmail.com>
2023-08-23 14:31:09 +0300

    [projectNew] сделала задачу 13

---
+++ project-13/wwwroot/scripts/main.js
@@ -0,0 +1,12 @@
+let ul = document.getElementById('ul');
+
+let liFirst = document.querySelector('ul > li:first-child');
+let li2 = document.createElement('li');
+li2.innerHTML = '2';
+liFirst.append(li2);
+
+let liLast = document.querySelector('ul > li:last-child');
+let li3 = document.createElement('li');
+li3.innerHTML = '3';
+liLast.before(li3);
+
