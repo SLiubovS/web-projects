11a64b030696c84876fa889c2a0623892e627b44
Liubov <gareevaliubov@gmail.com>
2023-09-19 16:29:53 +0300

    [projectNew] 33 задача

---
+++ project-33/wwwroot/scripts/main.js
@@ -0,0 +1,37 @@
+// нашли два имеющихся дива
+let slider = document.getElementById('slider'); // серая полоса для синего бегунка
+let thumb = document.getElementsByClassName('thumb')[0]; //синий бегунок
+
+thumb.onmousedown = function(event) {
+
+    let shiftX = event.clientX - thumb.getBoundingClientRect().left;
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+    function onMouseMove(event) {
+        let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
+
+        // курсор вышел из слайдера => оставить бегунок в его границах.
+        if (newLeft < 0) {
+            newLeft = 0;
+        }
+        let rightEdge = slider.offsetWidth - thumb.offsetWidth;
+        if (newLeft > rightEdge) {
+            newLeft = rightEdge;
+        }
+
+        thumb.style.left = newLeft + 'px';
+    }
+
+    function onMouseUp() {
+        document.removeEventListener('mouseup', onMouseUp);
+        document.removeEventListener('mousemove', onMouseMove);
+    }
+
+};
+
+thumb.ondragstart = function() {
+    return false;
+};
+
