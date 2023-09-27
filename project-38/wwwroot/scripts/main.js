1e58cb7731b5abff6960c4904d2804782fdf0801
Liubov <gareevaliubov@gmail.com>
2023-09-27 14:10:05 +0300

    [projectNew] 34 задача, осталось 2 вопроса 35 задача - есть частичное решение, но работает с задержкой
    36 - кажись работает так, как надо...
    37 - тоже работает
    38 - решена

---
+++ project-38/wwwroot/scripts/main.js
@@ -0,0 +1,31 @@
+/**
+ * Проверка видимости элемента (в видимой части страницы)
+ * Достаточно, чтобы верхний или нижний край элемента был виден
+ */
+function isVisible(elem) {
+
+    let coords = elem.getBoundingClientRect();
+    let heightAll = document.documentElement.clientHeight; // для сокращения, высота окна минус прокрутка
+    let topV = coords.top > 0 && coords.top < heightAll; // проверка, виден ли верхний край элемента?
+    let bottomV = coords.bottom > 0 && coords.bottom < heightAll; // проверка на видимость нижнего края нужна, на случай
+    // очень быстрой прокрутки
+    return topV || bottomV;
+}
+
+function showVisible() {
+    for (let img of document.querySelectorAll('img')) {
+        let realSrc = img.dataset.src;
+        if (!realSrc) continue;
+
+        if (isVisible(img) === true) {
+
+            img.src = realSrc;
+            img.dataset.src = '';
+        }
+    }
+
+}
+
+showVisible();
+window.addEventListener('scroll', showVisible);
+
