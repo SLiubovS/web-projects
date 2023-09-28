be1a96453e6d8638d115ca68de70cb9dbb054bf8
Liubov <gareevaliubov@gmail.com>
2023-09-28 17:00:59 +0300

    [projectNew] 34 задача, осталось 2 вопроса 35 задача - есть частичное решение, но работает с задержкой
    36 - кажись работает так, как надо...
    37 - тоже работает
    38 - решила
    39 - решила
    40 - работает, как надо, но в f12 отображается ошибка - показать Диме

---
+++ project-40/wwwroot/scripts/main.js
@@ -0,0 +1,45 @@
+// ...ваш код...
+// Заметьте: <textarea> должен иметь class="edit"
+// my.css содержит стиль, чтобы <textarea> и <div> были одного размера
+
+let view = document.getElementById("view");
+let textarea = document.createElement("textarea");
+view.addEventListener('mousedown', mousedown);
+view.addEventListener('blur', blur);
+
+function mousedown(elem) {
+    focus(elem);
+}
+
+function focus(elem) {
+
+    textarea.id = "view";
+    textarea.className = "edit";
+    document.body.append(textarea);
+    textarea.addEventListener('blur', blur);
+    textarea.innerHTML = view.innerHTML;
+    view.remove();
+    textarea.onkeydown = function () {
+        if (event.code === 'Enter') {
+            blur(elem);
+        }
+    }
+}
+
+function blur() {
+
+    let textarea = document.getElementById("view");
+    let div = document.createElement("div");
+
+    view.id = "view";
+    view.className = "view";
+    document.body.append(view);
+    end();
+    textarea.remove();
+
+    function end() {
+        view.innerHTML = textarea.value;
+
+    }
+}
+
