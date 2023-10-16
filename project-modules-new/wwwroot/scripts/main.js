4344599edfb3030126728e2e9436c45b1b9cfa3c
Dmitry Solovyev <solov.da@gmail.com>
2023-10-16 20:01:05 +0300

    [modules] отработка экспорта и импорта

--- project-modules-new/wwwroot/scripts/main.js
+++ project-modules-new/wwwroot/scripts/main.js
@@ -1,6 +1,8 @@
-import {scripts1} from './scripts1.js';
-import {scripts2} from './scripts2.js';
+import {scripts1, scripts11} from './scripts1.js';
+import * as scr from './scripts2.js';
 
 scripts1();
-scripts2();
+scr.scripts2();
+scripts11();
+scr.scripts11();
 console.log("Я main.js");
--- project-modules-new/wwwroot/scripts/scripts2.js
+++ project-modules-new/wwwroot/scripts/scripts2.js
@@ -1,3 +1,7 @@
 export function scripts2() {
     console.log("Я scripts2.js");
 }
+
+export function scripts11() {
+    console.log("Я функция scripts11 из файлика scripts2.js");
+}
