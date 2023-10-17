632484f2c1d7d03bc5ad188c63289585575bd453
Dmitry Solovyev <solov.da@gmail.com>
2023-10-17 18:48:53 +0300

    [types] разобрали типы данных

---
+++ project-types/wwwroot/scripts/main.js
@@ -0,0 +1,47 @@
+let nam = 10;
+
+console.log(typeof nam);
+
+let str = "строка";
+
+console.log(typeof str);
+
+let obj = {name: "name"};
+
+console.log(typeof obj);
+
+let func = function () {};
+
+console.log(typeof func);
+
+let und = undefined;
+
+console.log(typeof und);
+
+let n = null;
+
+console.log(typeof n);
+
+let bigI = 10n;
+
+console.log(typeof bigI);
+
+let bigI2 = BigInt(20);
+
+console.log(typeof bigI2);
+
+let id = Symbol("id");
+let id2 = Symbol("id");
+
+console.log(typeof id);
+console.log(typeof id2);
+
+console.log(id);
+console.log(id2);
+
+console.log(id.description);
+console.log(id2.description);
+
+let bool = true;
+
+console.log(typeof bool);
