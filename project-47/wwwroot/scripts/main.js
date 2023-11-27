c13c5bc1979fe74bdafb5d00b722df75b29481c7
Люба <gareevaliubov@gmail.com>
2023-11-27 14:53:10 +0300

    [master]  попытка решить 47 задачу с решебником

--- project-47/wwwroot/scripts/main.js
+++ project-47/wwwroot/scripts/main.js
@@ -1,3 +1,29 @@
+async function getUsers(names) {
 
+    let array = []; // создали пустой массив
 
+    for (let name in names) {
 
+        let arr = fetch(`https://api.github.com/users/${name}`).then(
+            successResponse => {
+                if (successResponse.status !== 200) {
+                    return null;
+                } else {
+                    return successResponse.json();
+                }
+            },
+            failResponse => {
+                return null;
+            }
+        );
+        array.push(arr);
+    }
+    let result = await Promise.all(array);
+
+    console.log(`arr: ${arr}, array: ${array}, result: ${result}`);
+
+    return result;
+}
+
+getUsers(`https://api.github.com/users/SLiubovS`);
+getUsers(`https://api.github.com/users/solov-da`);
