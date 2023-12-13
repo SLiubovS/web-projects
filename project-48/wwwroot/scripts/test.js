67e9c8044a94e2b8cf5432ab60d7cdc3613e4b42
Люба <gareevaliubov@gmail.com>
2023-12-13 16:50:30 +0300

    [master]  проект 48, написание тестов

--- project-48/wwwroot/scripts/test.js
+++ project-48/wwwroot/scripts/test.js
@@ -1,12 +1,55 @@
-it("Возводит x в степень n", function() {
+
+describe("row", function () {
+
     let x = 5;
+    let result;
 
-    let result = x;
-    assert.equal(pow(x, 1), result);
+    describe(`возводит 5 в степень 1`, function () {
+        let result = x;
+        it(`5 в степени 1 будет ${result}`, function () {
+            assert.equal(pow(x, 1), result);
+        });
+    });
+        describe(`возводит 5 в степень 2`, function () {
+            let result = x * x;
+            it(`5 в степени 2 будет ${result}`, function () {
+                assert.equal(pow(x, 2), result);
+            });
+        });
+            describe(`возводит 5 в степень 3`, function () {
+                let result = x * x * x;
+                it(`5 в степени 3 будет ${result}`, function () {
+                    assert.equal(pow(x, 3), result);
+                });
+            });
 
-    result *= x;
-    assert.equal(pow(x, 2), result);
-
-    result *= x;
-    assert.equal(pow(x, 3), result);
 });
+
+
+
+//
+//
+// Что не так с этим тестом?
+//     важность: 5
+// Что не так в нижеприведённом тесте функции pow?
+//
+//     it("Возводит x в степень n", function() {
+//         let x = 5;
+//
+//         let result = x;
+//         assert.equal(pow(x, 1), result);
+//
+//         result *= x;
+//         assert.equal(pow(x, 2), result);
+//
+//         result *= x;
+//         assert.equal(pow(x, 3), result);
+//     });
+// P.S. Тест не содержит синтаксических ошибок и успешно проходит.
+
+
+
+
+
+
+
