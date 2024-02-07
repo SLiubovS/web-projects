6f023dcf1b7a30e8372e843cd81938a03f0a084f
Люба <gareevaliubov@gmail.com>
2024-02-07 18:27:33 +0300

    [master] project-50 доработки main

--- project-50/wwwroot/main.js
+++ project-50/wwwroot/main.js
@@ -1,5 +1,7 @@
 let url = "http://localhost:5000/api/Files";
 let newUrl = "http://localhost:5000/api/Files/";
+let tbody = document.getElementById("tbody");
+let formFile = document.getElementById("formFile");
 
 // раздел получение списка всех пользователей
 fetch(url, {
@@ -17,25 +19,71 @@ fetch(url, {
             response
                 .text()
                 .then(text => JSON.parse(text)) // text => JSON.parse(text) или (тоже самое) : function(text) { return JSON.parse(text); }
-                .then(enumerationUsers);
+                .then(enumerationFiles);
 
         } else {
             alert("Ошибка HTTP: " + response.status);
         }
     });
 
+// раздел добавления 1 пользователя
+fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'multipart/form-data'
+    },
+    body: JSON.stringify(formFile)
+})
+    .then(response => {
+        if (response.ok) {
+            response
+                .text()
+                .then(text => JSON.parse(text));
+            createRow(formFile.id, formFile.name);
+        } else {
+            alert(`Ошибка HTTP: ${response.status}`);
+        }
+    });
 
 
 
+function createRow(id, name) {
 
+    let tr = document.createElement("tr"); // создали новую строку
+    tr.className = "tr-download tr-delete";
 
+    let td1 = document.createElement("td"); // создали новые поля строки
+    let td2 = document.createElement("td"); // создали новые поля строки
+    let td3 = document.createElement("td"); // создали новые поля строки
+    let td4 = document.createElement("td");
+
+    let buttonDownload = document.createElement("i");
+    buttonDownload.className = "btn btn-warning fa-solid fa-pen";
+    buttonDownload.alt = "Скачать";
+    td3.append(buttonDownload);
+
+    let buttonDelete = document.createElement("i");
+    buttonDelete.className = "btn btn-danger fa-solid fa-user-xmark";
+    buttonDelete.alt = "Удалить";
+    td4.append(buttonDelete);
+
+    td1.textContent = id;
+    td2.value = name;
+
+    tr.append(td1);
+    tr.append(td2);
+    tr.append(td3);
+    tr.append(td4);
+
+    tbody.append(tr);
+}
 
 
 // ф-ия для получения всех пользователей
-function enumerationUsers(array) {
+function enumerationFiles(array) {
 
     for (let index = 0; index <= array.length - 1; index++) {
 
-        createRow(array[index].id, array[index].firstName, array[index].lastName);
+        createRow(array[index].id, array[index].name);
     }
 }
