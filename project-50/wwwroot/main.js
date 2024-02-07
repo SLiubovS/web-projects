2d104eb4760a776782374f077f4d8990dc80e11f
Люба <gareevaliubov@gmail.com>
2024-02-07 17:36:28 +0300

    [master] project-50 добавила main

--- project-50/wwwroot/main.js
+++ project-50/wwwroot/main.js
@@ -1,3 +1,41 @@
 let url = "http://localhost:5000/api/Files";
 let newUrl = "http://localhost:5000/api/Files/";
 
+// раздел получение списка всех пользователей
+fetch(url, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+})
+    .then(response => {
+        if (response.ok) {
+
+            // функциональный стиль программирования
+            // когда результат 1 функции передается следующей вниз по цепочке
+
+            response
+                .text()
+                .then(text => JSON.parse(text)) // text => JSON.parse(text) или (тоже самое) : function(text) { return JSON.parse(text); }
+                .then(enumerationUsers);
+
+        } else {
+            alert("Ошибка HTTP: " + response.status);
+        }
+    });
+
+
+
+
+
+
+
+
+// ф-ия для получения всех пользователей
+function enumerationUsers(array) {
+
+    for (let index = 0; index <= array.length - 1; index++) {
+
+        createRow(array[index].id, array[index].firstName, array[index].lastName);
+    }
+}
